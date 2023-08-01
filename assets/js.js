4b594433e457306408cdde8020d9ef04d4dab21b
kayefrancisco <106609187+kayefrancisco@users.noreply.github.com>
2023-08-01 12:42:15 +0800

    Add files via upload

---
+++ assets/js.js
@@ -0,0 +1,40 @@
+//https://www.javatpoint.com/codeigniter-database-select-record
+
+
+
+
+
+//if else kunyari yung crops is num 1 lang all number 1 have different color e.latlng
+
+
+
+
+
+
+
+//map onclick
+
+
+
+
+
+//sss//dropdown
+
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    var input = document.getElementById("others-text");
+    var input2 = input.value;
+
+    //Mag input din na mag upercase or lower case basta iisa d na pwde input
+    if (x === "others") {
+     
+      input.style.display = "block";
+    } else {
+      input.style.display = "none";
+    }
+    
+  }
+
+ 
+//ajax
+
